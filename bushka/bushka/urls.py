aa64251d5b122575524e0365446792d5235e48b6
laimsas <laimispas@gmail.com>
2022-06-21 14:41:03 +0300

    antras

--- bushka/bushka/urls.py
+++ bushka/bushka/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import static
+from django.urls import path, include
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('', include('bushka_site.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ bushka/bushka_site/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BushkaSiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bushka_site'
