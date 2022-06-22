7e70e028dcd9473245431a931e979c37a6c502ff
laimsas <laimispas@gmail.com>
2022-06-22 13:34:00 +0300

    su seaborn plotu

--- bushka/bushka_site/urls.py
+++ bushka/bushka_site/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'bushka_site'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('show_recoil/', views.show_recoil, name='show_recoil'),
 ]
--- bushka/bushka_site/views.py
+++ bushka/bushka_site/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+import openpyxl
+from io import BytesIO
+import base64
+from django.conf import settings
 
 # Create your views here.
 
+
+
+
+def show_recoil():
+    df = pd.read_excel(r'C:\Users\Laimsas\Documents\pythonptu1\bushka\bushka\bushka\bushka_site\static\bushka_site\recoil_multi.xlsx', index_col=0)
+    recoil = sns.scatterplot(data = df,x = 'groza_X', y = 'groza_Y')
+    fig = recoil.figure
+    recoil_file = BytesIO() 
+    fig.savefig(recoil_file, format='png')
+    encoded_file = base64.b64encode(recoil_file.getvalue())
+    return encoded_file
+
 def index(request):
-    
-    return render(request, 'base.html')
+    return render(request, 'bushka_site/show_recoil.html',{'show_recoil':show_recoil().decode('utf-8')})
