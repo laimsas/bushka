1fb3c19f46a4f1e3afc920ba134310a39baa109d
laimsas <laimispas@gmail.com>
2022-06-23 12:24:49 +0300

    pridetas admin funkcionalumas

--- bushka/bushka_site/admin.py
+++ bushka/bushka_site/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categorie, Weapon
 
 # Register your models here.
+
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ('categorie', 'description')
+    
+
+class WeaponAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'categorie')
+
+
+admin.site.register(Categorie)
+admin.site.register(Weapon)
--- bushka/bushka_site/models.py
+++ bushka/bushka_site/models.py
@@ -10,6 +10,9 @@ class Categorie(models.Model):
     categorie = models.CharField(_('categorie'), max_length=100)
     description = models.CharField(_('description'), max_length=200)
 
+    def __str__(self):
+        return f'{str(self.categorie)} - {self.description}'
+
 
 class Weapon(models.Model):
     name = models.CharField(_('name'), max_length=100)
@@ -17,4 +20,7 @@ class Weapon(models.Model):
     recoil_x = models.CharField(_('recoil_x'), max_length=100)
     recoil_y = models.CharField(_('recoil_y'), max_length=100)
     image = models.ImageField(_('image'), upload_to='bushka_site/img', null=True, blank=True)
-    categorie = models.ForeignKey(Categorie, on_delete=models.SET_NULL, null=True, related_name='categories', verbose_name=_('autorius'))
+    categorie = models.ForeignKey(Categorie, on_delete=models.SET_NULL, null=True, related_name='categories', verbose_name=_('category'))
+
+    def __str__(self):
+        return f'{str(self.name)} - {self.description} - {self.categorie}'
