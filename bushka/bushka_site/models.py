9b9b91b76d62c3a6274806169c8c33ab12f92f88
laimsas <laimispas@gmail.com>
2022-06-23 10:13:04 +0300

    bugai

--- bushka/bushka_site/models.py
+++ bushka/bushka_site/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+from tinymce.models import HTMLField
 
 # Create your models here.
+
+
+class Categorie(models.Model):
+    categorie = models.CharField(_('categorie'), max_length=100)
+    description = models.CharField(_('description'), max_length=200)
+
+
+class Weapon(models.Model):
+    name = models.CharField(_('name'), max_length=100)
+    description = models.CharField(_('description'), max_length=200)
+    recoil_x = models.CharField(_('recoil_x'), max_length=100)
+    recoil_y = models.CharField(_('recoil_y'), max_length=100)
+    image = models.ImageField(_('image'), upload_to='bushka_site/img', null=True, blank=True)
+    categorie = models.ForeignKey(Categorie, on_delete=models.SET_NULL, null=True, related_name='categories', verbose_name=_('autorius'))
